ce2a6e4aef383569d906c978d886344c6b4cafda
Nyika Sánor <nyikasandor@hotmail.com>
2021-10-08 19:20:42 +0200

    Added test code

--- Gh-AccesViolation/Main.cpp
+++ Gh-AccesViolation/Main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include <thread>
+#include <mutex>
+
+std::mutex cout_mutex;
+
+int some_func(int i) {
+  std::lock_guard<std::mutex> guard(cout_mutex);
+  std::cout << "some_func\n";
+  return i*3;
+}
+
+void call_some_func() {
+  for (int i = 0; i < 1e3; ++i) {
+    some_func(i);
+  }
+}
 
 int main() {
-  std::cout << "main\n";
+  std::thread t1(call_some_func);
+  std::thread t2(call_some_func);
+  call_some_func();
+  {
+    std::lock_guard<std::mutex> guard(cout_mutex);
+    std::cout << "main\n";
+  }
+
+
+  t1.join();
+  t2.join();
   return 0;
 }
